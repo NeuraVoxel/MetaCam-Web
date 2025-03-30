741182a58b33d3dc4476b9e6a4d847043161ea4b
ue007 <871245242@qq.com>
2025-03-30 09:53:01 +0800

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import PointCloud from './components/PointCloud';
 
+function LoginPage() {
+  const handleStart = () => {
+    window.location.href = '/point-cloud';
+  };
+
+  return (
+    <div className="login-container">
+      <h1>MetaCam 登录</h1>
+      <button onClick={handleStart}>开始作业</button>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-        <PointCloud 
-          url="ws://192.168.1.11:9090"  // ROS WebSocket服务器地址
-          // topic="/point_cloud"  // 点云数据的话题名称
-          topic="/lidar_out"  // 点云数据的话题名称
-          width={1200}
-          height={800}
-        />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/point-cloud" element={
+          <div className="App">
+            <PointCloud 
+              url="ws://192.168.1.11:9090"
+              topic="/lidar_out"
+              width={1200}
+              height={800}
+            />
+          </div>
+        } />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
